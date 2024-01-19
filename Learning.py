1777e7932ab473879ab4f8408410a95e16362210
nickpedri <nickmirandabr@gmail.com>
2024-01-19 17:08:11 -0600

    added failsafe

--- Learning.py
+++ Learning.py
@@ -1,9 +1,11 @@
+import pyautogui
 import pyautogui as pag  # Use pag to control mouse
 import random  # Use random to add randomness to movements
 import time  # Use time to time mouse movements
 
 # Create loop that will repeat 10 times to test simple mouse movement
 for n in range(1, 11):
+    pyautogui.FAILSAFE = True
     rx = random.randint(-5, 5)
     ry = random.randint(-5, 5)
     print(f' {n}.)   {rx}, {ry}')

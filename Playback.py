67631ed63db4190686ee3f2269d79e81879cc8ec
nickpedri <nickmirandabr@gmail.com>
2024-01-24 18:42:54 -0600

    update playback to take in account processing time

--- Playback.py
+++ Playback.py
@@ -18,6 +18,31 @@ def countdown(seconds=10):
     print(' now!')
 
 
+def convert_key(key):
+    key_map = {
+        'alt_l': 'altleft',
+        'alt_r': 'altright',
+        'alt_gr': 'altright',
+        'caps_lock': 'capslock',
+        'ctrl_l': 'ctrlleft',
+        'ctrl_r': 'ctrlright',
+        'page_down': 'pagedown',
+        'page_up': 'pageup',
+        'shift_l': 'shiftleft',
+        'shift_r': 'shiftright',
+        'num_lock': 'numlock',
+        'print_screen': 'printscreen',
+        'scroll_lock': 'scrolllock'}
+
+    # example: 'Key.F9' should return 'F9', 'w' should return as 'w'
+    cleaned_key = key.replace('Key.', '')
+
+    if cleaned_key in key_map:
+        return key_map[cleaned_key]
+
+    return cleaned_key
+
+
 def play_actions(filename):
     previous_position = None
     script_dir = os.path.dirname(__file__)
@@ -25,27 +50,28 @@ def play_actions(filename):
     with open(filepath, 'r') as jsonfile:
         data = json.load(jsonfile)
         for index, action in enumerate(data):
+            start_time = time.time()
             if action['button'] == 'Key.f10':
                 break
             # Perform action
             elif action['type'] == 'KeyDown':
-                key = action['button']
-                key = key[4:] if key[:4] == 'Key.' else key
+                key = convert_key(action['button'])
+                # key = key[4:] if key[:4] == 'Key.' else key
                 pag.keyDown(key)
             elif action['type'] == 'KeyUp':
-                key = action['button']
-                key = key[4:] if key[:4] == 'Key.' else key
+                key = convert_key(action['button'])
+                # key = key[4:] if key[:4] == 'Key.' else key
                 pag.keyDown(key)
 
             elif action['type'] == 'clickDown':
                 previous_position = (action['pos'][0], action['pos'][1])
-                pag.moveTo(action['pos'][0] + p(), action['pos'][1] + p(), duration=r(0.25, 0.70))
+                pag.moveTo(action['pos'][0] + p(-5, 5), action['pos'][1] + p(-5, 5), duration=r(0.25, 0.70))
                 pag.mouseDown()
             elif action['type'] == 'clickUp':
                 if previous_position == (action['pos'][0], action['pos'][1]):
                     pag.mouseUp()
                 else:
-                    pag.moveTo(action['pos'][0] + p(), action['pos'][1] + p(), duration=r(0.25, 1.00))
+                    pag.moveTo(action['pos'][0] + p(-5, 5), action['pos'][1] + p(-5, 5), duration=r(0.25, 1.00))
                     pag.mouseUp()
 
             # Sleep until next action
@@ -53,9 +79,16 @@ def play_actions(filename):
                 next_action = data[index + 1]
             except IndexError:
                 break
-            elapsed_time = next_action['time'] - action['time']
-            if elapsed_time >= 0:
-                time.sleep(elapsed_time)
+            elapsed_time = time.time() - start_time
+            wait_time = next_action['time'] - action['time']
+            if wait_time >= 0:
+                wait_time -= elapsed_time
+                if wait_time < 0:
+                    wait_time = 0
+                if action['type'] == 'clickDown':
+                    time.sleep(wait_time)
+                else:
+                    time.sleep(wait_time + r(0, 0.30))
             else:
                 raise Exception('Unexpected action ordering.')
 
@@ -63,7 +96,10 @@ def play_actions(filename):
 def main():
     countdown(3)
     initialize_pag()
-    play_actions('actions_test_01.json')
+    for n in range(1, 11):
+        play_actions('actions_test_01.json')
+        time.sleep(55 + r(0, 5))
+        print(f'Loop {n} done!')
 
 
 main()

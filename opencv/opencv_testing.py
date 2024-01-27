1cb0462fada9dc45c98ccdf0a6751212af90c95e
nickpedri <nickmirandabr@gmail.com>
2024-01-27 16:11:38 -0600

    created function to track character in game

--- opencv/opencv_testing.py
+++ opencv/opencv_testing.py
@@ -4,11 +4,30 @@ import time
 
 
 # Capturing a smaller piece of the screen is much faster
-start_time = time.time()
-screenshot2 = pag.screenshot('s2.png', region=(550, 370, 500, 500))
-end_time = time.time()
-print(end_time - start_time)
+# start_time = time.time()
+# screenshot2 = pag.screenshot('s1.png', region=(550, 370, 500, 500))
+# end_time = time.time()
 
-s1 = cv.imread('s2.png', cv.IMREAD_UNCHANGED)
-cv.imshow('full screen', s1)
-cv.waitKey()
+
+def locate_simon():
+    pag.screenshot('s1.png', region=(550, 370, 500, 500))
+    simon_area = cv.imread('s1.png', cv.IMREAD_UNCHANGED)
+    simon = cv.imread('simon_templeton.png', cv.IMREAD_UNCHANGED)
+    result = cv.matchTemplate(simon_area, simon, cv.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+
+    simon_w = simon.shape[1] / 2
+    simon_h = simon.shape[0] / 2
+
+    simon_location = (max_loc[0] + 550 + simon_w, max_loc[1] + 370 + simon_h)
+    # print(simon_location)
+    return simon_location
+
+
+start_time = time.time()
+elapsed_time = 0
+
+while elapsed_time < 10:
+    pag.moveTo(locate_simon())
+    elapsed_time = time.time() - start_time
+    print(elapsed_time)

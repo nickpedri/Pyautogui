8bb18c0456f4d7771f165d5de503b4edac50db79
nickpedri <nickmirandabr@gmail.com>
2024-04-24 16:55:10 -0600

    updated functions

--- video_capture.py
+++ video_capture.py
@@ -2,29 +2,38 @@ import os
 import cv2 as cv
 import functions as f
 import time
+from functions import WindowCapture, Vision
 
+# initialize the WindowCapture class
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+wincap = WindowCapture()
+# initialize the Vision class
+vision_limestone = Vision('search_img.png')
 
 
 def capture_runelite():
-    # wincap = f.WindowCapture('Runelite - Crad Booper')
-    wincap = f.WindowCapture(area=(1650, 700, 250, 350))
+
+    loop_time = time.time()
 
     while True:
-        t = time.time()
-        screenshot = wincap.capture_window()  # Capture screenshot
-        # screenshot = f.take_screenshot()  # Capture screenshot
-        matches = f.find_spots('search_img.png', 0.70)
-        if matches:
-            rectangles = f.create_rectangles('search_img.png', matches)
-            f.draw_rectangles(screenshot, rectangles, False)
-        cv.imshow('Computer Vision', screenshot)
-        print(f'{round(1/(time.time() - t))} fps')
 
+        # get an updated image of the game
+        screenshot = wincap.get_screenshot()
+
+        # display the processed image
+        points = vision_limestone.find(screenshot, 0.45, 'rectangles')
+        # points = vision_gunsnbottle.find(screenshot, 0.7, 'points')
+
+        # debug the loop rate
+        print('FPS {}'.format(1 / (time.time() - loop_time)))
+        loop_time = time.time()
+
+        # press 'q' with the output window focused to exit.
+        # waits 1 ms every loop to process key presses
         if cv.waitKey(1) == ord('q'):
             cv.destroyAllWindows()
             break
 
+    print('Done.')
 
 capture_runelite()
